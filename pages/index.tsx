6d7bb53004eec2f401f610273e213d7067b21cdf
Samrakshan Karki <samrakshankarki1@gmail.com>
2023-01-30 12:56:43 +0545

    added chakra ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,11 @@ import { writeUserData } from "../firebase/dboperations";
 const DashboardPage=()=>{
   useEffect(()=>{
     console.log("this runs on mount")
-    
-    let userId = 123;
-    let name = "John Doe";
-    let email = "johndoe@example.com";
-    let imageUrl = "https://example.com/image.jpg";
-    writeUserData(userId, name, email, imageUrl);
+    // let userId = 789;
+    // let name = "John Doe";
+    // let email = "johndoe@example.com";
+    // let imageUrl = "https://example.com/image.jpg";
+    // writeUserData(userId, name, email, imageUrl);
   },[])
  
   return (
